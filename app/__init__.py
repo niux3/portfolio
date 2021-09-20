fb2f9749fe22569b6a8ae9031f3eb709a80e4b47
Renaud Bourdeau <renaudbourdeau@gmail.com>
2021-09-21 00:50:35 +0200

    feat: init sqlalchemy, config, migrate, blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,24 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_blueprint import Blueprint
+from app.config import config
+
+db = SQLAlchemy()
 
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(config['default'])
+    config['default'].init_app(app)
 
-    @app.route('/')
-    def index():
-        return "hey"
+    db.init_app(app)
+    migrate = Migrate()
+
+    from app.portfolio.models import Function, Portfolio
+    migrate.init_app(app, db)
+
+    from app.portfolio import views as portfolio_views
+    app.register_blueprint(portfolio_views.bp)
 
     return app
