81eeed259069924db9eb3964804b9e3856a5ba7f
Renaud Bourdeau <renaudbourdeau@gmail.com>
2021-09-21 22:16:03 +0200

    feat(portfolio): models is ok

--- app/portfolio/models.py
+++ app/portfolio/models.py
@@ -16,6 +16,28 @@ class Function(db.Model):
         return "<Function %r>" % self.name
 
 
+portfolio_technology = db.Table(
+    'portfolios_technologies',
+
+    db.Column('id', db.Integer, primary_key=True),
+    db.Column('technologies_id', db.Integer, db.ForeignKey('technologies.id')),
+    db.Column('portfolios_id', db.Integer, db.ForeignKey('portfolios.id'))
+)
+
+
+class Technology(db.Model):
+    __tablename__ = 'technologies'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32), nullable=False)
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return "<Technology %r>" % self.name
+
+
 class Portfolio(db.Model):
     __tablename__ = 'portfolios'
 
@@ -27,6 +49,7 @@ class Portfolio(db.Model):
     modified = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
     online = db.Column(db.SmallInteger, default=1)
     functions_id = db.Column(db.Integer, db.ForeignKey('functions.id'))
+    technologies = db.relationship("Technology", secondary=portfolio_technology, backref=db.backref('portfolios', lazy="dynamic"))
 
     def __init__(self, *args, **kwargs):
         super(Post, self).__init__(*args, **kwargs)
