108b6b3682eaa693ec52df944faefc6a5fb33c8a
Renaud Bourdeau <renaudbourdeau@gmail.com>
2021-09-21 23:33:29 +0200

    feat(portfolio): init more methods

--- app/portfolio/views.py
+++ app/portfolio/views.py
@@ -1,8 +1,45 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+from app.portfolio.forms import CommonForm
+from app import db
+from app.portfolio.models import Function
 
 bp = Blueprint('portfolio', __name__)
 
 
 @bp.route('/')
-def index():
-    return render_template('portfolio/index.html')
+def index_portfolio():
+    ctx = {
+        'online': True,
+        'suffixe_url': '_portfolio'
+    }
+    return render_template('portfolio/index.html', **ctx)
+
+
+@bp.route('/create-portfolio')
+def create_portfolio():
+    ctx = {
+        'suffixe_url': '_portfolio'
+    }
+    return render_template('portfolio/create.html')
+
+
+@bp.route('/function')
+def index_function():
+    ctx = {
+        'suffixe_url': '_function',
+        'rows': Function.query.all()
+    }
+    return render_template('portfolio/index.html', **ctx)
+
+
+@bp.route('/create-function')
+def create_function():
+    form = CommonForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        db.session.add(Function(name=form.name.data.lower()))
+        db.session.commit()
+        return redirect(url_for('portfolio.index_function'))
+    ctx = {
+        'form': form,
+    }
+    return render_template('portfolio/create.html', **ctx)
