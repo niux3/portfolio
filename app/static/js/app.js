1e28ff33a803852220ac03eaf9655b72277bfdee
Renaud Bourdeau <renaudbourdeau@gmail.com>
2021-09-23 01:05:43 +0200

    feat(portfolio): factorize js

---
+++ app/static/js/app.js
@@ -0,0 +1,23 @@
+$(function(){
+    if($('.slug').length){
+        $('.name').on('keyup', function(){
+            $('.slug').val($(this).val().slugify());
+        });
+    }
+    if($('.description').length){
+        $('.description').on('keyup', function(){
+            if($(this).val() === 'lorem'){
+                $(this).val("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet laborum illum maxime iusto minima reiciendis praesentium labore ab, sed sapiente officia enim consequuntur atque non rerum nostrum quibusdam rem molestiae!")
+            }
+        });
+    }
+    if($('.toggle-online.btn-danger, .delete').length){
+        $('.toggle-online.btn-danger, .delete').on('click', function(e){
+            var answer = $(this).hasClass('delete')? "supprimer" : 'mettre hors ligne';
+            var response = confirm("Voulez vous vraiment "+ answer +" cet article ?");
+            if(!response){
+                e.preventDefault();
+            }
+        });
+    }
+});
