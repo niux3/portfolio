d9e83f29efe0922d90e4e51d719f9e663c06246a
Renaud Bourdeau <renaudbourdeau@gmail.com>
2021-09-24 14:42:25 +0200

    feat(images): create & and upload images

--- app/images/forms.py
+++ app/images/forms.py
@@ -8,8 +8,8 @@ from app.portfolio.models import Portfolio
 
 class UploadCreateForm(FlaskForm):
     portfolios = QuerySelectField(query_factory=lambda: Portfolio.query.all())
-    name = StringField('name', validators=[InputRequired()], render_kw={"class": "form-control", 'autofocus': 'true'})
-    description = StringField('description', validators=[InputRequired()], widget=TextArea(), render_kw={"class": "form-control description"})
+    name = StringField('name', validators=[InputRequired()], render_kw={"class": "form-control", 'autofocus': 'true', 'value': "bla"})
+    description = StringField('description', validators=[InputRequired()], widget=TextArea(), render_kw={"class": "form-control description"}, default="blabla")
     online = BooleanField('en ligne', render_kw={"class": "form-check-input", "value": "1"})
     thumbnail = BooleanField('miniature', render_kw={"class": "form-check-input"})
     upload = FileField('upload')
--- app/images/views.py
+++ app/images/views.py
@@ -1,9 +1,12 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from app.portfolio.models import Portfolio
+from app.images.models import Image
 from app.images.forms import UploadCreateForm
 from werkzeug.utils import secure_filename
-from app import config
+from app import config, db
+from app.config import basedir
 import os
+import uuid
 
 bp = Blueprint('images', __name__)
 
@@ -22,14 +25,32 @@ def index():
 def create():
     form = UploadCreateForm()
     if request.method == 'POST' and form.validate_on_submit():
+        dir = form.portfolios.data.slug
+        base_path = os.path.join(config['default'].UPLOAD_FOLDER, dir)
+        if not os.path.isdir(base_path):
+            os.mkdir(base_path)
         file = request.files['upload']
-        path = os.path.join(config['default'].UPLOAD_FOLDER, secure_filename(file.filename))
+        name, ext = file.filename.split('.')
+        name = uuid.uuid4()
+        file.filename = f"{form.portfolios.data.id}__{name}.{ext.lower()}"
+        path = os.path.join(base_path, secure_filename(file.filename))
+        data = {
+            'portfolios_id': form.portfolios.data.id,
+            'url': path.replace(basedir, ''),
+            'name': form.name.data,
+            'description': form.description.data,
+            'online': form.online.data,
+            'thumbnail': form.thumbnail.data,
+        }
         file.save(path)
+        image = Image(**data)
+        db.session.add(image)
+        db.session.commit()
         return redirect(url_for('images.create'))
     ctx = {
         'form': form
     }
-    return render_template('views/edit.html', **ctx)
+    return render_template('views/edit-images.html', **ctx)
 
 
 @bp.route('/update-images', methods=['GET', 'POST'])
