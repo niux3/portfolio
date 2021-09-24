4b3b77210f0ff530a90c2dc065e28e245bebf4d4
Renaud Bourdeau <renaudbourdeau@gmail.com>
2021-09-24 02:40:08 +0200

    feat(images): begin uploading file

---
+++ app/images/forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms.validators import InputRequired
+from wtforms import StringField, BooleanField, IntegerField, FileField, HiddenField
+from wtforms.widgets import TextArea
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
+from app.portfolio.models import Portfolio
+
+
+class UploadCreateForm(FlaskForm):
+    portfolios = QuerySelectField(query_factory=lambda: Portfolio.query.all())
+    name = StringField('name', validators=[InputRequired()], render_kw={"class": "form-control", 'autofocus': 'true'})
+    description = StringField('description', validators=[InputRequired()], widget=TextArea(), render_kw={"class": "form-control description"})
+    online = BooleanField('en ligne', render_kw={"class": "form-check-input", "value": "1"})
+    thumbnail = BooleanField('miniature', render_kw={"class": "form-check-input"})
+    upload = FileField('upload')
--- app/images/views.py
+++ app/images/views.py
@@ -1,16 +1,35 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+from app.portfolio.models import Portfolio
+from app.images.forms import UploadCreateForm
+from werkzeug.utils import secure_filename
+from app import config
+import os
 
 bp = Blueprint('images', __name__)
 
 
 @bp.route('/index-images')
 def index():
-    return 'index images !'
+    ctx = {
+        'rows': Portfolio.query.all(),
+        'prefixe_url': 'images',
+        'suffixe_url': ''
+    }
+    return render_template('views/index.html', **ctx)
 
 
 @bp.route('/create-images', methods=['GET', 'POST'])
 def create():
-    return 'create images !'
+    form = UploadCreateForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        file = request.files['upload']
+        path = os.path.join(config['default'].UPLOAD_FOLDER, secure_filename(file.filename))
+        file.save(path)
+        return redirect(url_for('images.create'))
+    ctx = {
+        'form': form
+    }
+    return render_template('views/edit.html', **ctx)
 
 
 @bp.route('/update-images', methods=['GET', 'POST'])
