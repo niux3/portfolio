9374818a0b9fca5ec86c23b720a7b8ee26997423
Renaud Bourdeau <renaudbourdeau@gmail.com>
2023-02-09 00:32:35 +0100

    feat: button nav animated

--- frontoffice/src/Preheader.svelte
+++ frontoffice/src/Preheader.svelte
@@ -5,5 +5,9 @@
 
 <div class="wrap-nav">
     <img on:click={onSelectFirstSlide} id="logo" src="logo.svg?v=1" alt="">
-    <button on:click={onCloseNav}><i class="fa-solid fa-bars"></i></button>
+    <a href="/" on:click|preventDefault={onCloseNav} class="burger-button">
+        <div class="bar1"></div>
+        <div class="bar2"></div>
+        <div class="bar3"></div>
+    </a>
 </div>
