2ab4320e5ff5e480a46bc5ecf5df73e5f945d9a1
Renaud Bourdeau <renaudbourdeau@gmail.com>
2023-02-14 11:11:50 +0100

    feat: mise en page project

--- frontoffice/src/Article.svelte
+++ frontoffice/src/Article.svelte
@@ -1,4 +1,6 @@
 <script>
+    import {onMount} from 'svelte'
+    import {debounce} from './helpers'
     import Footer from './Footer.svelte'
     import PreHeader from './Preheader.svelte'
     import About from './About.svelte'
@@ -11,8 +13,26 @@
     export let data
 
     $: view = is_not_first? Project : About
+
+    onMount(()=>{
+        let lastScrollTop = 0,
+            clsOnScrollUp = 'on-scroll-up'
+        document.querySelector('article').addEventListener('scroll', e =>{
+            debounce(()=>{
+                let currentScrollTop = e.target.scrollTop
+                console.log('>>', currentScrollTop)
+                if(currentScrollTop > lastScrollTop && e.target.classList.contains(clsOnScrollUp)){
+                    e.target.classList.remove(clsOnScrollUp)
+                }else if(currentScrollTop < lastScrollTop && !e.target.classList.contains(clsOnScrollUp)){
+                    e.target.classList.add(clsOnScrollUp)
+                }
+                lastScrollTop = currentScrollTop
+            }, 800)()
+        })
+    })
+
 </script>
-<article class={!is_not_first? "first": ''}>
+<article class={!is_not_first? 'first on-scroll-up': 'on-scroll-up'}>
     <PreHeader onSelectFirstSlide={onSelectFirstSlide} onCloseNav={onCloseNav} />
     <svelte:component this={view} data={data} len={len} />
     <Footer onSelectFirstSlide={onSelectFirstSlide} />
--- frontoffice/src/Project.svelte
+++ frontoffice/src/Project.svelte
@@ -4,7 +4,7 @@
 </script>
 <header>
     <div class="description">
-        <p class="counter">{data.index} - {len}</p>
+        <p class="counter">✺&nbsp;&nbsp;{data.index} - {len}</p>
         <h1 class="name">{data.name}</h1>
         <div class="wrap">
             <div class="cartouche">
@@ -27,12 +27,26 @@
                     </dd>
                 </dl>
             </div>
-                <ul>
-                    {#each data.technologies as technology, i}
-                        <li>{technology}</li>
-                    {/each}
-                </ul>
+<!--                <ul>-->
+<!--                    {#each data.technologies as technology, i}-->
+<!--                        <li>{technology}</li>-->
+<!--                    {/each}-->
+<!--                </ul>-->
         </div>
     </div>
 </header>
-<div class="content"></div>
+<div class="content">
+    <img src="https://placekitten.com/g/914/533" alt="">
+    <div class="info">
+        <aside></aside>
+        <div class="resume">
+            <div>Touch my tail, i shred your hand purrrr put toy mouse in food bowl run out of litter box at full speed climb a tree, wait for a fireman jump to fireman then scratch his face so jump on fridge. I could pee on this if i had the energy i see a bird i stare at it i meow at it i do a wiggle come here birdy friends are not food.</div>
+            <ul>
+                <li><strong>client</strong><span>ACME</span></li>
+                <li><strong>année</strong><span>2021</span></li>
+                <li><strong>type</strong><span>frontend</span></li>
+                <li><strong>lieu</strong><span>Paris</span></li>
+            </ul>
+        </div>
+    </div>
+</div>
