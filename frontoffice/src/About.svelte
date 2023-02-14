3d354f90ee23a0a9fe044909980db06d68f3759a
Renaud Bourdeau <renaudbourdeau@gmail.com>
2023-02-14 19:53:34 +0100

    feat: add html view about

--- frontoffice/src/About.svelte
+++ frontoffice/src/About.svelte
@@ -1,3 +1,117 @@
-<div class="content">
-    <p>à propos de</p>
+<script>
+    let now = new Date()
+    let year = now.getFullYear() - 2010
+</script>
+<div class="about">
+    <div class="intro">
+        <div>
+            <h1>Web développeur frontend et backend à Paris</h1>
+            <p>Bonjour, ancien infographiste, je suis désormais un professionnel dans le web. Mon expertise se situe principalement dans les technologies du web où j'excelle avec aisance dans le monde professionnel depuis {year} ans. À mon actif, j'ai plus de 50 beaux projets et j'ai été récompensé 5 fois. </p>
+            <button></button>
+        </div>
+    </div>
+    <div class="resume">
+        <div class="aboutGrid top">
+            <p>à propos de moi</p>
+            <p>Je suis passionné par tout ce qui a à voir avec la conception numérique. J'aime travailler avec des agences et des personnes enthousiastes qui veulent résoudre des problèmes grâce à une conception suivant les standards.</p>
+            <img src="" alt="Renaud Bourdeau" />
+        </div>
+        <div class="aboutGrid bottom">
+            <div>
+                <h2>Développement frontend</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus dolorem mollitia quam quia sint? Atque aut illum odio quia rem saepe? Ad at aut doloribus illo, obcaecati omnis pariatur! Est?</p>
+            </div>
+            <div>
+                <h2>Développement backend</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt earum error harum iusto laboriosam minima nesciunt officiis quidem reprehenderit soluta. Amet architecto dolor et, id laboriosam possimus reprehenderit saepe sapiente!</p>
+            </div>
+            <div>
+                <h2>Conception visuelle</h2>
+                <p>En utilisant mon expertise, je m'assure qu'une identité visuelle puisse être utilisée dans toute son étendue. Non seulement intemporel, mais utilisable dans le monde numérique d'aujourd'hui.</p>
+            </div>
+            <div>
+                <button>portfolio</button>
+            </div>
+        </div>
+    </div>
+    <div class="experience">
+        <p>expérience</p>
+        <ul>
+            <li>
+                <span>2022</span>
+                <span>développeur backend</span>
+                <span>John Paul</span>
+            </li>
+            <li>
+                <span>2021 - 2022</span>
+                <span>développeur front/back</span>
+                <span>la Maison de la Radio</span>
+            </li>
+            <li>
+                <span>2019 - 2021</span>
+                <span>développeur front/back</span>
+                <span>Journal LePoint</span>
+            </li>
+            <li>
+                <span>2019</span>
+                <span>développeur front/back</span>
+                <span>Carenity</span>
+            </li>
+            <li>
+                <span>2018 - 2019</span>
+                <span>développeur front/back</span>
+                <span>Groupe Creative</span>
+            </li>
+            <li>
+                <span>2016 - 2018</span>
+                <span>développeur front/back</span>
+                <span>Prodigious</span>
+            </li>
+            <li>
+                <span>2015 - 2016</span>
+                <span>développeur front</span>
+                <span>Fullsix</span>
+            </li>
+            <li>
+                <span>2014 - 2015</span>
+                <span>développeur front</span>
+                <span>Groupe Bazarchic</span>
+            </li>
+            <li>
+                <span>2010 - 2013</span>
+                <span>développeur front</span>
+                <span>Business Lab</span>
+            </li>
+        </ul>
+    </div>
+    <div class="awwards">
+        <p>récompenses</p>
+        <ul>
+            <li>
+                <span>2016</span>
+                <span>Top Com Or</span>
+                <span>Renault Kadjarquest</span>
+            </li>
+            <li>
+                <span>2013</span>
+                <span>Top Com Or</span>
+                <span>Leroy Merlin</span>
+            </li>
+            <li>
+                <span>2012</span>
+                <span>Top Com Or</span>
+                <span>Peugeot</span>
+            </li>
+            <li>
+                <span>2012</span>
+                <span>Top Com Or</span>
+                <span>Peugeot 4008</span>
+            </li>
+            <li>
+                <span>2011</span>
+                <span>Top Com Or</span>
+                <span>Areva</span>
+            </li>
+        </ul>
+    </div>
 </div>
