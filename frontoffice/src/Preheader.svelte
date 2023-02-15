ba75f220cf5c41ecd3d28b44ae5b6e6a9623c513
Renaud Bourdeau <renaudbourdeau@gmail.com>
2023-02-15 19:47:58 +0100

    feat: scale burger

--- frontoffice/src/Preheader.svelte
+++ frontoffice/src/Preheader.svelte
@@ -1,6 +1,19 @@
 <script>
+    import {onMount} from 'svelte'
     export let onSelectFirstSlide
     export let onCloseNav
+
+    onMount(()=>{
+        let scale_burger = ()=>{
+            let $burgerButton = document.querySelector('.burger-button')
+            $burgerButton.style.transform = `scale(${parseFloat((window.innerWidth * 1 / 1920).toFixed(2))})`
+        }
+        scale_burger()
+        window.addEventListener('resize', e =>{
+            scale_burger()
+        })
+    })
+
 </script>
 
 <div class="wrap-nav">
