f0a687d71dccfb17af1ee7c6d1a2e64f5e566bf9
Renaud Bourdeau <renaudbourdeau@gmail.com>
2023-02-21 02:25:06 +0100

    feat: responsive footer

--- frontoffice/src/Footer.svelte
+++ frontoffice/src/Footer.svelte
@@ -2,8 +2,8 @@
     export let onSelectFirstSlide
 </script>
 <footer>
-    <a href="/" on:click|preventDefault={onSelectFirstSlide}>à propos de moi</a>
-    <a href="mailto:renaudbourdeau@gmail.com" target="_blank">me contacter</a>
-    <a href="https://www.linkedin.com/in/renaud-bourdeau-7639b944/" target="_blank">mon CV</a>
-    <a href="https://www.linkedin.com/in/renaud-bourdeau-7639b944/" target="_blank">LinkedIn</a>
+    <a href="/" on:click|preventDefault={onSelectFirstSlide} class="fa-solid fa-address-card"><span>à propos de moi</span></a>
+    <a href="mailto:renaudbourdeau@gmail.com" target="_blank" class="fa-solid fa-envelope"><span>me contacter</span></a>
+    <a href="https://www.linkedin.com/in/renaud-bourdeau-7639b944/" target="_blank" class="fa-solid fa-user"><span>mon CV</span></a>
+    <a href="https://www.linkedin.com/in/renaud-bourdeau-7639b944/" target="_blank" class="fa-brands fa-linkedin"><span>LinkedIn</span></a>
 </footer>
