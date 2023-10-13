b506b74ed2d6fa2ec5af9d999af88a977a5329e3
Renaud Bourdeau <renaud.bourdeau@radiofrance.com>
2023-10-13 19:20:25 +0200

    feat: project slide continue

--- frontoffice/src/components/Project.svelte
+++ frontoffice/src/components/Project.svelte
@@ -7,56 +7,56 @@
         "https://placekitten.com/g/914/533"
 </script>
 <article>
-<header>
-    <div class="description">
-        <p class="counter">✺&nbsp;&nbsp;{index} - {len}</p>
-        <h1 class="name">{row.name}</h1>
-        <div class="wrap">
-            <div class="cartouche">
-                <dl>
-                    <dt>type</dt>
-                    <dd>
-                        <span>{row.function}</span>
-                    </dd>
-                </dl>
-                <dl>
-                    <dt>description</dt>
-                    <dd>
-                        <span>{row.activity_name}</span>
-                    </dd>
-                </dl>
-                <dl>
-                    <dt>année</dt>
-                    <dd>
-                        <span>{row.year}</span>
-                    </dd>
-                </dl>
+    <header>
+        <div class="description">
+            <p class="counter">✺&nbsp;&nbsp;{index} - {len}</p>
+            <h1 class="name">{row.name}</h1>
+            <div class="wrap">
+                <div class="cartouche">
+                    <dl>
+                        <dt>type</dt>
+                        <dd>
+                            <span>{row.function}</span>
+                        </dd>
+                    </dl>
+                    <dl>
+                        <dt>description</dt>
+                        <dd>
+                            <span>{row.activity_name}</span>
+                        </dd>
+                    </dl>
+                    <dl>
+                        <dt>année</dt>
+                        <dd>
+                            <span>{row.year}</span>
+                        </dd>
+                    </dl>
+                </div>
+                    <ul>
+                        {#each row.technologies as technology, i}
+                            <li>{technology}</li>
+                        {/each}
+                    </ul>
             </div>
+        </div>
+    </header>
+    <div class="content">
+            <div class="wrap-thumb">
+                <img src={thumb} alt="" loading="lazy">
+            </div>
+        <div class="info">
+            <aside>
+                <a href={row.url} target="_blank">{row.url} <span class="fa-solid fa-caret-right"></span></a>
+            </aside>
+            <div class="resume">
+                <div>{row.description}</div>
                 <ul>
-                    {#each row.technologies as technology, i}
-                        <li>{technology}</li>
-                    {/each}
+                    <li><strong>client</strong><span>ACME</span></li>
+                    <li><strong>année</strong><span>2021</span></li>
+                    <li><strong>type</strong><span>frontend</span></li>
+                    <li><strong>lieu</strong><span>Paris</span></li>
                 </ul>
+            </div>
         </div>
     </div>
-</header>
-<div class="content">
-        <div class="wrap-thumb">
-            <img src={thumb} alt="" loading="lazy">
-        </div>
-    <div class="info">
-        <aside>
-            <a href={row.url} target="_blank">{row.url} <span class="fa-solid fa-caret-right"></span></a>
-        </aside>
-        <div class="resume">
-            <div>{row.description}</div>
-            <ul>
-                <li><strong>client</strong><span>ACME</span></li>
-                <li><strong>année</strong><span>2021</span></li>
-                <li><strong>type</strong><span>frontend</span></li>
-                <li><strong>lieu</strong><span>Paris</span></li>
-            </ul>
-        </div>
-    </div>
-</div>
 </article>
