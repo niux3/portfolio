94018b8869a66d4aaff3dc019e9a7edfa35edc3b
Renaud Bourdeau <renaud.bourdeau@radiofrance.com>
2023-10-14 08:46:40 +0200

    feat: cursor behavior on click

--- frontoffice/src/libs/cursor/Cursor.js
+++ frontoffice/src/libs/cursor/Cursor.js
@@ -9,12 +9,14 @@ export default class Cursor{
         window.addEventListener('mousemove', e =>{
             if(e.target.classList.contains('event-cursor')){
                 //console.log(">> ", e.target)
-                $cursor.style.left = `${e.target.getBoundingClientRect().left}px`
-                $cursor.style.top = `${e.target.getBoundingClientRect().top}px`
-                $cursor.style.width = `${e.target.getBoundingClientRect().width}px`
-                $cursor.style.height = `${e.target.getBoundingClientRect().height}px`
-                $cursor.style.borderRadius = `6px`
-                $cursor.style.transform = `translate(0, 0)`
+                $cursor.style = [
+                    `left: ${e.target.getBoundingClientRect().left}px`,
+                    `top: ${e.target.getBoundingClientRect().top}px`,
+                    `width:${e.target.getBoundingClientRect().width}px`,
+                    `height:${e.target.getBoundingClientRect().height}px`,
+                    `border-radius: 6px`,
+                    `transform: translate(0, 0)`,
+                ].join(';')
             }else{
                 $cursor.style = [
                     `left: ${e.pageX}px`,
@@ -28,5 +30,13 @@ export default class Cursor{
         })
         document.body.addEventListener('mouseenter', e => $cursor.style.display = 'block')
         document.body.addEventListener('mouseleave', e => $cursor.style.display = 'none')
+        document.body.addEventListener('click', e => $cursor.style = [
+            `left: ${e.pageX}px`,
+            `top: ${e.pageY}px`,
+            `width:10px`,
+            `height:10px`,
+            `border-radius: 50%`,
+            `transform: translate(-50%, -50%)`,
+        ].join(';'))
     }
 }
