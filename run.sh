5391c9ffda42d9616120e690fdb37f9f9eff8798
Renaud Bourdeau <renaud.bourdeau@radiofrance.com>
2023-10-18 15:35:30 +0200

    feat: contrib

--- run.sh
+++ run.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-if [ ! -d "venv" ]
-then
-    virtualenv --python python3 venv && . venv/bin/activate && pip install -r requirements.txt
-fi
-. venv/bin/activate && export FLASK_ENV=development && export FLASK_APP=app && flask run -p 8000
+pipenv shell && export FLASK_ENV=development && export FLASK_APP=app && flask run -p 8000
 
