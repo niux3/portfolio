34ecc28766144caf17889616714b60f0f7e68b33
niux3 <renaudbourdeau@gmail.com>
2023-11-17 07:55:23 +0100

    feat: rm old folder

--- frontoffice/src/old/Footer.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    export let onSelectFirstSlide
-</script>
-<footer>
-    <a href="/" on:click|preventDefault={onSelectFirstSlide} class="fa-solid fa-address-card"><span>à propos de moi</span></a>
-    <a href="mailto:renaudbourdeau@gmail.com" target="_blank" class="fa-solid fa-envelope"><span>me contacter</span></a>
-    <a href="https://www.linkedin.com/in/renaud-bourdeau-7639b944/" target="_blank" class="fa-solid fa-user"><span>mon CV</span></a>
-    <a href="https://www.linkedin.com/in/renaud-bourdeau-7639b944/" target="_blank" class="fa-brands fa-linkedin"><span>LinkedIn</span></a>
-</footer>
