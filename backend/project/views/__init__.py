97017c5f1df8ee5abaa898c8f008fd1ca3575b89
niux3 <renaudbourdeau@gmail.com>
2025-03-30 20:56:16 +0200

    feat: crud technologies

--- backend/project/views/__init__.py
+++ backend/project/views/__init__.py
@@ -1,2 +1,3 @@
 from backend.project.views.activity import bp as activity_bp
 from backend.project.views.function import bp as function_bp
+from backend.project.views.technology import bp as technology_bp
---
+++ backend/project/views/technology.py
@@ -0,0 +1,28 @@
+from flask import Blueprint
+from backend.project.models import Technology
+from backend.project.forms import CommonForm
+from backend.core.libs.base_views import BaseView
+
+
+prefix_bp = 'technology'
+bp = Blueprint(prefix_bp, __name__, url_prefix='/technologies')
+
+@bp.route('/index.html')
+def index():
+    fields = {
+        'Nom' : 'name',
+    }
+    return BaseView.index(Technology, prefix_bp, fields, "une technologie")
+
+@bp.route('/ajouter.html', methods=['GET', 'POST'])
+def add():
+    return BaseView.add(CommonForm, Technology, prefix_bp)
+
+@bp.route('/<int:id>-supprimer.html')
+def destroy(id):
+    return BaseView.destroy(id, Technology, prefix_bp)
+
+@bp.route('/<int:id>-editer.html', methods=['GET', 'POST'])
+def edit(id):
+    return BaseView.edit(id, Technology, CommonForm, prefix_bp)
+
