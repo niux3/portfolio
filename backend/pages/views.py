8f9c182861ee791aecba693fba55b53c05fa9344
niux3 <renaudbourdeau@gmail.com>
2025-04-05 01:30:00 +0200

    feat: dynamize page index

--- backend/pages/views.py
+++ backend/pages/views.py
@@ -1,8 +1,15 @@
 from flask import render_template, Blueprint
+from backend.project.models import Project, Technology
 
 
 bp = Blueprint('pages', __name__)
 
 @bp.route('/')
 def index():
-    return render_template('pages/index.html')
+    ctx = {
+        "object_list": {
+            "projects": Project.query.all(),
+            "technologies": Technology.query.all()
+        }
+    }
+    return render_template('pages/index.html', **ctx)
