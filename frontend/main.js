b878dd053a702c67c944333d52834fa6e59854ed
niux3 <renaudbourdeau@gmail.com>
2025-04-06 18:33:30 +0200

    feat: prepare contact

--- frontend/main.js
+++ frontend/main.js
@@ -2,5 +2,12 @@ import './scss/index.scss'
 
 
 (()=>{
-    console.log('ok vitejs !')
+    let $fields = [
+        document.querySelector('input[name="phone"]'),
+        document.querySelector('input[name="date_appointment"]'),
+        document.querySelector('input[name="hour_appointment"]'),
+    ]
+    document.querySelector('input[name=appointement]').addEventListener('change', e =>{
+        $fields.forEach($field => e.target.checked? $field.removeAttribute('disabled') : $field.setAttribute('disabled', true))
+    })
 })()
