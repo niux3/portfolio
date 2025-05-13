61057a0c2138ecee973eed21814acef3f65c4373
niux3 <renaudbourdeau@gmail.com>
2025-05-13 14:58:10 +0200

    feat: begin error contact form

--- frontend/main.js
+++ frontend/main.js
@@ -1,7 +1,8 @@
 import './scss/index.scss'
+import Validator from '@niuxe/validator'
 
 
-(()=>{
+window.addEventListener('DOMContentLoaded', () =>{
     let $fields = [
         document.querySelector('input[name="phone"]'),
         document.querySelector('input[name="date_appointment"]'),
@@ -10,4 +11,6 @@ import './scss/index.scss'
     document.querySelector('input[name=appointement]').addEventListener('change', e =>{
         $fields.forEach($field => e.target.checked? $field.removeAttribute('disabled') : $field.setAttribute('disabled', true))
     })
-})()
+    let validate = new Validator({})
+    validate.form()
+})
