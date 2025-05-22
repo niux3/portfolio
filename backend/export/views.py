1369bddf19622d16dfaedd8e3f75e3c40ed3569e
niux3 <renaudbourdeau@gmail.com>
2025-05-22 18:22:44 +0200

    feat: w3c validation and export view

---
+++ backend/export/views.py
@@ -0,0 +1,33 @@
+import subprocess
+import json
+from flask import render_template, Blueprint, jsonify
+from backend.core.config import config
+from backend.project.models import Project, Technology
+
+
+bp = Blueprint('export', __name__)
+
+@bp.route('/export.html')
+def export():
+    result = subprocess.check_call('npm run build', shell=True)
+    dist_folder = config.BASEDIR.parent / 'dist'
+    static_folder = dist_folder / 'static'
+
+    with open(str(static_folder / '.vite' / 'manifest.json'), encoding='utf-8') as f:
+        manifest_data = json.load(f)
+
+    ctx = {
+        "layout_template": "base_export.html",
+        "js_file": manifest_data.get('frontend/main.js').get('file'),
+        "css_file": manifest_data.get('frontend/scss/index.scss').get('file'),
+        "object_list": {
+            "projects": Project.query.filter(Project.online == 1).all(),
+            "technologies": Technology.query.filter(Technology.online == 1).all(),
+        }
+    }
+    html = render_template('pages/index.html', **ctx)
+    with open(str(dist_folder / 'index.html'), "w", encoding="utf-8") as f:
+        f.write(html)
+    return jsonify({
+        "msg": html
+    })
