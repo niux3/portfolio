978a3695405720564fdce7a206b329740e44ce22
niux3 <renaudbourdeau@gmail.com>
2025-05-23 13:20:07 +0200

    feat: css contact

--- frontend/main.js
+++ frontend/main.js
@@ -91,15 +91,43 @@ window.addEventListener('load', () =>{
 
     let validate = new Validator(optionValidator)
 
+
     document.querySelector('input[name=appointement]').addEventListener('change', e =>{
         for(let $field of $fields){
             $field.setAttribute('disabled', true)
             validate.removeRequireField($field)
+            if(document.querySelectorAll('#errorAppointment .error').length > 0){
+                document.querySelectorAll('#errorAppointment .error').forEach(el => el.remove())
+
+            }
             if(e.target.checked){
                 $field.removeAttribute('disabled')
                 validate.addRequireField($field)
             }
         }
     })
+    validate.addRules('checkphone', value => /^0[1-8][ .-]?(\d{2}[ .-]?){4}$/.test(value))
+    validate.addRules('datemin', (value)=>{
+        let now = new Date(),
+            dateUser = new Date(value)
+
+        dateUser.setHours(0)
+        dateUser.setMinutes(0)
+        now.setHours(0)
+        now.setMinutes(0)
+
+        return now.getTime() >= dateUser.getTime()
+    })
+    validate.addRules('businessday', (value)=> [0, 6].includes(new Date(value).getDay()))
+    validate.addRules('betweenhour', (...args)=>{
+        let [value, params_validate] = args,
+            {params, error} = params_validate,
+            [min, max] = params.split(';'),
+            [hour, minutes] = value.split(':')
+
+        return parseInt(min, 10) > parseInt(hour, 10) || parseInt(max, 10) < parseInt(hour, 10)
+    })
+
+    console.log(validate.check('bla', 'checkphone'))
     validate.form()
 })
