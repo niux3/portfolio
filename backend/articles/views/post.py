d733512383f5a8f10aae2e260973f3603227216a
niux3 <renaudbourdeau@gmail.com>
2025-05-28 22:47:28 +0200

    feat: begin CRUD posts

--- backend/articles/views/post.py
+++ backend/articles/views/post.py
@@ -1,9 +1,13 @@
 from flask import render_template, Blueprint
 from markdown import markdown
 from backend.core.config import config
+from backend.core.libs.base_views import BaseView
+from backend.articles.models import Post
+from backend.articles.forms import PostForm
 
 
-bp = Blueprint('posts', __name__, url_prefix='/articles')
+prefix_bp = 'posts'
+bp = Blueprint(prefix_bp, __name__, url_prefix='/articles')
 
 @bp.route('/voir.html')
 def show():
@@ -25,3 +29,29 @@ def show():
         ]
     }
     return render_template('articles/show.html', **ctx)
+
+@bp.route('/index.html')
+def index():
+    fields = {
+        'slug' : 'slug',
+        'status' : 'status',
+    }
+    return BaseView.index(Post, prefix_bp, fields, "un article")
+
+@bp.route('/ajouter.html', methods=['GET', 'POST'])
+def add():
+    form = PostForm()
+    if form.validate_on_submit():
+        return 'valide !'
+    ctx = {
+        'form': form
+    }
+    return 'ajouter'
+
+@bp.route('/<int:id>-supprimer.html')
+def destroy(id):
+    return 'destoy ' + id
+
+@bp.route('/<int:id>-editer.html', methods=['GET', 'POST'])
+def edit(id):
+    return 'edit ' + id
