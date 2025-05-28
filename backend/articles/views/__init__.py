abe9c9184631cf1a5c5eaba9ae7738c61ad4e116
niux3 <renaudbourdeau@gmail.com>
2025-05-28 12:44:59 +0200

    feat: rename blueprint instance

--- backend/articles/views/__init__.py
+++ backend/articles/views/__init__.py
@@ -1,2 +1,2 @@
-from backend.articles.views.tag import bp as bp_tag
-from backend.articles.views.post import bp as bp_post
+from backend.articles.views.tag import bp as tag_bp
+from backend.articles.views.post import bp as post_bp
