42864cfab098798659f49dd101b170e92a81ce30
niux3 <renaudbourdeau@gmail.com>
2025-06-17 17:07:06 +0200

    feat: add category model

---
+++ backend/articles/models/category.py
@@ -0,0 +1,23 @@
+from slugify import slugify
+from backend.core.libs.serializer_mixin import SerializerMixin
+from backend import db
+
+
+class Category(db.Model, SerializerMixin):
+    __tablename__ = 'articles_categories'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    slug = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+
+    def generate_slug(self):
+        self.slug = ''
+        if self.name:
+            self.slug = slugify(self.name)
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return "<Category %r>" % self.slug
