038d6bd9b380e46883471961552419b2389f03f2
niux3 <renaudbourdeau@gmail.com>
2025-07-04 04:07:11 +0200

    fix: no button in footer

--- backend/core/context_processors.py
+++ backend/core/context_processors.py
@@ -5,6 +5,7 @@ def inject_footer_flag():
     def context():
         endpoints_without_footer = {
             "posts.show",
+            "backup_articles.export_html",
         }
         return {
             "display_buttons_footer": request.endpoint not in endpoints_without_footer
