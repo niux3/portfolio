ee3a0d904b9c972bb4999beec05805b45613786e
niux3 <renaudbourdeau@gmail.com>
2025-07-08 17:04:02 +0200

    feat: add terser plugin for vitejs

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ dotenv.config()
 
 export default defineConfig(({ command }) => ({
     root: path.join(__dirname),
+    base: '/',
     // Chemin remplacé dans les ressources compilées
     // (correspond au chemin public http vers le dossier contenant assetsDir)
     // base: command === 'build' ? `/themes/${process.env.WP_THEME}/assets/` : '/',
@@ -14,11 +15,17 @@ export default defineConfig(({ command }) => ({
     // Dossier dans lequel on place les ressources statiques non compilées, qui seront copiées vers outDir
     // publicDir: 'resources/static',
     build: {
+        tminify: command === 'build' ? 'terser' : false,
+        terserOptions: {
+            keep_classnames: true,
+            keep_fnames: true,
+        },
         outDir: 'public', // Dossier destination du build
         assetsDir: 'static', // Sous-dossier dans lequel placer les assets (js, css) générés par Vite
         emptyOutDir: false, // Vide le dossier destination à chaque build
         manifest: true, // Génère un manifeste json listant les chemins vers les assets
         rollupOptions: {
+            treeshake: false,
             input: {
                 // index: path.resolve(__dirname, 'index.html'), // Point d'entrée HTML
                 app: path.resolve(__dirname, 'frontend/js/main.js'), // Point d'entrée HTML
@@ -34,5 +41,8 @@ export default defineConfig(({ command }) => ({
     },
     server: {
         origin: 'http://localhost:5173',
-    }
+    },
+    optimizeDeps: {
+        exclude: ['swup', '@swup/a11y-plugin'],
+    },
 }))
