d92012005464420c2809cfbf451f0bae62737c08
niux3 <renaudbourdeau@gmail.com>
2026-01-19 18:05:29 +0100

    feat: write expérience

--- backend/project/forms/project_form.py
+++ backend/project/forms/project_form.py
@@ -5,18 +5,24 @@ from wtforms.ext.sqlalchemy.fields import QuerySelectMultipleField, QuerySelectF
 from wtforms.widgets import TextArea, CheckboxInput, ListWidget
 from backend.project.models import Technology, Function, Activity
 
+
 class ProjectForm(FlaskForm):
-    name = StringField('name', validators=[InputRequired()], render_kw={'autofocus': 'true', "autocomplete": "off"})
+    name = StringField('name', validators=[InputRequired()], render_kw={
+                       'autofocus': 'true', "autocomplete": "off"})
     slug = StringField('slug', render_kw={"readonly": True})
     url = StringField('url', validators=[InputRequired()])
     year = StringField('year', validators=[InputRequired()])
-    description = StringField('description', validators=[InputRequired()], widget=TextArea())
-    color = StringField('couleur', validators=[InputRequired()], render_kw={"type": "color"})
+    description = StringField('description', validators=[InputRequired(
+    )], widget=TextArea(), render_kw={'style': 'min-height: 200px; resize: vertical;'})
+    color = StringField('couleur', validators=[
+                        InputRequired()], render_kw={"type": "color"})
     customer = StringField('customer', validators=[InputRequired()])
     location = StringField('location', validators=[InputRequired()])
     online = BooleanField('en ligne', render_kw={"value": "1"})
-    activity = QuerySelectField('Activity', query_factory=lambda: Activity.query.all(), get_label='name')
-    function = QuerySelectField( 'Fonction', query_factory=lambda: Function.query.all(), get_label='name')
+    activity = QuerySelectField(
+        'Activity', query_factory=lambda: Activity.query.all(), get_label='name')
+    function = QuerySelectField(
+        'Fonction', query_factory=lambda: Function.query.all(), get_label='name')
     technologies = QuerySelectMultipleField(
         'technologies',
         query_factory=lambda: Technology.query.all(),
