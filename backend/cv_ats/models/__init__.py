6cd1c9cb4e384f660d4e1fcb6d153bba895ba356
niux3 <renaudbourdeau@gmail.com>
2026-07-01 15:20:52 +0200

    feat: add contactract type model

--- backend/cv_ats/models/__init__.py
+++ backend/cv_ats/models/__init__.py
@@ -1,3 +1,4 @@
 from backend.cv_ats.models.cv_data import CvData
 from backend.cv_ats.models.cv_position import CvPosition
+from backend.cv_ats.models.cv_contract_type import CvContractType
 from backend.cv_ats.models.cv_work import CvWork
