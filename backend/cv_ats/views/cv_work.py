48370879a180ce78626083bbcb7cb7945324e544
niux3 <renaudbourdeau@gmail.com>
2026-07-02 17:53:48 +0200

    feat: upgrade view cv_work

--- backend/cv_ats/views/cv_work.py
+++ backend/cv_ats/views/cv_work.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash, url_for, redirect, request
 from backend.cv_ats.models import CvWork
 from backend.cv_ats.forms import CvWorkForm
 from backend.core.libs.base_views import BaseView
+from backend import db
 
 
 prefix_bp = 'cv_work'
@@ -11,14 +12,23 @@ bp = Blueprint(prefix_bp, __name__, url_prefix='/cv_work')
 @bp.route('/backoffice/index.html')
 def index():
     fields = {
-        'Nom': 'name',
+        'Client': 'customer',
+        'Année début': 'year_start',
+        'Année fin': 'year_end',
     }
-    return BaseView.index(CvWork.query.all(), prefix_bp, fields, "expérience")
+    return BaseView.index(CvWork.query.order_by(CvWork.year_end.desc()).all(), prefix_bp, fields, "expérience")
 
 
 @bp.route('/backoffice/ajouter.html', methods=['GET', 'POST'])
 def add():
     form = CvWorkForm()
+    if form.validate_on_submit() and request.method == "POST":
+        work = CvWork()
+        form.populate_obj(work)
+        db.session.add(work)
+        db.session.commit()
+        flash("Votre item a bien été ajouté", "success")
+        return redirect(url_for(f'{prefix_bp}.index'))
     ctx = {
         'form': form
     }
@@ -27,9 +37,30 @@ def add():
 
 @bp.route('/backoffice/<int:id>-supprimer.html')
 def destroy(id):
-    return 'destroy'
+    instance = CvWork.query.get_or_404(id)
+    db.session.flush()
+    db.session.delete(instance)
+    db.session.commit()
+    flash("Votre item a bien été supprimé", "success")
+    return redirect(url_for(f'{prefix_bp}.index'))
 
 
 @bp.route('/backoffice/<int:id>-editer.html', methods=['GET', 'POST'])
 def edit(id):
-    return 'edit'
+    instance = CvWork.query.get_or_404(id)
+    form = CvWorkForm(obj=instance)
+    if form.validate_on_submit():
+        form.populate_obj(instance)
+        try:
+            db.session.commit()
+            flash("Votre item a bien été modifié", "success")
+        except Exception as e:
+            db.session.rollback()
+            flash(f"Erreur lors de la mise à jour : {str(e)}", "danger")
+            print("Erreur SQLAlchemy :", e)
+        return redirect(url_for(f'{prefix_bp}.index'))
+    ctx = {
+        "instance": instance,
+        "form": form
+    }
+    return render_template('project/edit.html', **ctx)
