4e753b2d5be70dd64f8518c2414bdab3ab7d66e3
niux3 <renaudbourdeau@gmail.com>
2026-07-02 16:32:14 +0200

    feat: add form cv_work

---
+++ backend/cv_ats/forms/cv_work_form.py
@@ -0,0 +1,44 @@
+from flask_wtf import FlaskForm
+from wtforms.validators import InputRequired
+from wtforms import StringField, IntegerField
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
+from wtforms.widgets import TextArea
+from backend.project.models import Customer
+from backend.cv_ats.models import CvPosition, CvContractType
+
+
+class CvWorkForm(FlaskForm):
+    customer = QuerySelectField(
+        'Client',
+        query_factory=lambda: Customer.query.order_by(Customer.name).all(),
+        get_label='name',
+        validators=[InputRequired()]
+    )
+    position = QuerySelectField(
+        'Fonction',
+        query_factory=lambda: CvPosition.query.all(),
+        get_label='name'
+    )
+    contract_type = QuerySelectField(
+        'Contrat',
+        query_factory=lambda: CvContractType.query.all(),
+        get_label='name'
+    )
+    location = StringField(
+        'location',
+        validators=[InputRequired()],
+        render_kw={
+            'autofocus': 'true',
+            "autocomplete": "off"
+        }
+    )
+    year_start = IntegerField('year_start')
+    year_end = IntegerField('year_end', validators=[InputRequired()])
+    description = StringField(
+        'description',
+        validators=[InputRequired()],
+        widget=TextArea(),
+        render_kw={
+            'style': 'min-height: 200px; resize: vertical;'
+        }
+    )
